b51e09bcbdcf84f827ec9295e45758bbdb05ed4e
PedroC55 <plbc@ua.pt>
2023-02-11 23:22:42 +0000

    Update

--- client.py
+++ client.py
@@ -7,3 +7,15 @@ def __init__(self, address):
         self.dht_addr = address
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.logger = logging.getLogger("DHTClient")
+
+def get(self, key):
+        """ Retrieve key from DHT."""
+        msg = {"method": "GET", "args": {"key": key}}
+        pickled_msg = pickle.dumps(msg)
+        self.socket.sendto(pickled_msg, self.dht_addr)
+        pickled_msg, addr = self.socket.recvfrom(1024)
+        out = pickle.loads(pickled_msg)
+        if out["method"] != "ACK":
+            self.logger.error("Invalid msg: %s", out)
+            return None
+        return out["args"]
